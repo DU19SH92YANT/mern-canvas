1212839fed8e591fc37faa3e8976db230cbe4cad
Dushyant <du19sh92yant@gmail.com>
2024-05-11 02:27:00 -1200

    mern-canvas

---
+++ server.js
@@ -0,0 +1,44 @@
+const express = require('express')
+const dotenv = require("dotenv")
+const app = express()
+const cors = require('cors')
+const {  mongoose } = require('mongoose')
+const path = require('path')
+dotenv.config()
+if(process.env.NODE_ENV === "local"){
+    app.use(cors({
+        origin:"https://localhost:3000",
+        credentials: true
+    }))
+}else{
+    app.use(cors({
+        
+        credentials: true
+    }))
+}
+
+if(process.env.NODE_ENV === 'production'){
+    app.use(express.static(path.join(__dirname , './frontend/dist')))
+    app.get("*",(req , res)=>{
+        res.sendFile(path.resolve(__dirname, "./" , "frontend","dist", "index.html"))
+    })
+}
+
+const dbConnect = async ()=>{
+try {
+    if(process.env.NODE_ENV === "local"){
+        await mongoose.connect(process.env.LOCAL_DB_URI)
+    }else{
+    
+    }
+} catch (error) {
+    
+}
+}
+
+dbConnect()
+
+
+
+const port = process.env.PORT
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
